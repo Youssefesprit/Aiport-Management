e07d4adb1741be34a818b69a1258a262eda359f7
Youssefesprit <youssef.farhat@esprit.tn>
2024-02-04 21:03:58 +0100

    Ajoutez des fichiers projet.

---
+++ AM.UI.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using AM.Core.Domain;
+
+Console.WriteLine("Hello, World!");
+
+Plane plane = new Plane();
+plane.Capacity = 1;
+plane.ManufactureDate = DateTime.Now;
+plane.MyPlaneType = PlaneType.Boing;
+
+Plane plane2 = new Plane(PlaneType.Boing, 1,DateTime.Now) ;
+
+Plane plane3 = new Plane()
+{
+    Capacity = 1,
+    ManufactureDate = DateTime.Now,
+} ;
+
